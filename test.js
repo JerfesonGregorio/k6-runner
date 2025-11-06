8f98336baa0d7cda6ddf769d2d6e8ff982dd42b2
jerfeson.moreno.ext <jerfeson.moreno.ext@synchro.com.br>
2025-11-06 11:10:17 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+
+import { sleep } from 'k6';
+
+export const options = {
+    iterations: 10
+};
+
+export default function () {
+    
+    http.get('https://quickpizza.grafana.com');
+
+    sleep(1);
+}
